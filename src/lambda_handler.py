9364a8036c6f99cae28129d3e991aaef70b05ed2
Felddy <markf@geekpad.com>
2024-09-20 12:25:09 -0400

    Refactor handler to improve comments and add log level configuration

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -3,9 +3,12 @@
 # Standard Python Libraries
 import asyncio
 import logging
+import os
 
 # Third-Party Libraries
 from cyhy_kevsync import do_kev_sync
+
+CYHY_LOG_LEVEL = "CYHY_LOG_LEVEL"
 
 
 def handler(event, context) -> None:
@@ -18,11 +21,16 @@ def handler(event, context) -> None:
     :param context: The context in which the function is called.
     :return: A None response which means success.
     """
+    # Check if the log level is set in the environment. This is useful when we
+    # want to enable debug logs early for searching and loading configurations.
+    # Otherwise, the log level will be set once the configuration is loaded.
+    log_level = os.getenv(CYHY_LOG_LEVEL, None)
+
     try:
-        asyncio.run(do_kev_sync())
+        asyncio.run(do_kev_sync(arg_log_level=log_level))
     except Exception as err:
-        # TODO say more maybe?
+        # Log the exception and throw it to indicate failure of the Lambda.
         logging.exception(err)
 
-    # Return None to indicate success.
+    # Return None to indicate success of the Lambda.
     return None
