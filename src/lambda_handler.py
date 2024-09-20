59e4023f4539d2913845c807b7d6b880f6111d12
Felddy <markf@geekpad.com>
2024-09-20 12:07:32 -0400

    Refactor lambda_handler.py to improve comments and return success response

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -11,15 +11,18 @@ from cyhy_kevsync import do_kev_sync
 def handler(event, context) -> None:
     """Process the event and generate a response.
 
-    The event should have a task member that is one of the supported tasks.
+    The event contents are not evaluated.
 
     :param event: The event dict that contains the parameters sent when the function
                   is invoked.
     :param context: The context in which the function is called.
-    :return: The result of the action.
+    :return: A None response which means success.
     """
     try:
         asyncio.run(do_kev_sync())
     except Exception as err:
         # TODO say more maybe?
         logging.exception(err)
+
+    # Return None to indicate success.
+    return None
